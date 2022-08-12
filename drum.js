32364691cc8be0f6e8788af3b13901df7621b39c
Jarrian <89616705+godkingjay@users.noreply.github.com>
2022-08-12 17:31:32 +0800

    Add files via upload

---
+++ drum.js
@@ -0,0 +1,20 @@
+function removeTransition (e) {
+	if (e.propertyName !== 'transform') return;
+	e.target.classList.remove("play");
+}
+
+function playSound(e) {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+	if (!audio) return;
+
+	key.classList.add("play");
+	audio.currentTime = 0;
+	audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
